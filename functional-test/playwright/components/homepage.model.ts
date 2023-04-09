bf7ca6451d1c478e0480ad701456751cd751f6cf
rojadasaridev <rojadasariuk@gmail.com>
2023-04-10 00:42:58 +0100

    Updated homepage locator

--- functional-test/playwright/components/homepage.model.ts
+++ functional-test/playwright/components/homepage.model.ts
@@ -1,2 +1,15 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
+
+export class home {
+
+    static readonly MACBOOKAIR = '[data-globalnav-item-name="mac"]';
+
+    readonly macBookAir: Locator;
+
+
+    constructor(page: Page) {
+        this.macBookAir = page.locator(home.MACBOOKAIR);
+
+    }
+}
 
